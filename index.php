814eb5619bacf250c27e9924e1495bf65cd38844
JNALY <3530661536@qq.com>
2023-03-30 19:52:04 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 简洁清新的纯文字 Typecho 主题
+ * 
+ * @package Typecho Theme Matcha
+ * @author Eltrac
+ * @version 1.2.0
+ * @link https://github.com/BigCoke233/matcha
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+$this->need('includes/header.php'); 
+$this->need('includes/main.php'); 
+$this->need('includes/footer.php'); ?>
